84324bea58f89a9200c1a76c8360b586d26085b2
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-07-20 08:39:52 +0900

    loading routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import AppLayout from './theme/Layout.vue'
+import router from './router'
 
 console.log(AppLayout)
 
 const app = new Vue({
+  router,
   ...AppLayout
 })
 
-export { app }
+export { app, router }
