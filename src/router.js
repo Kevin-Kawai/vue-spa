b42b160fbbccb0b4ffecdbc0d3fdc0c50bf6ce3a
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-07-24 07:30:26 +0900

    history mode and router-link + scroll behavior

--- src/router.js
+++ src/router.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [{
-    path: '/', component: Category
-  }]
+  mode: 'history',
+  linkActiveClass: 'is-active',
+  scrollBehavior: (to, from, savedPosition) => ({ y: 0 }),
+  routes: [
+    { path: '/login', component: Login },
+    { path: '/', component: Category }
+  ]
 })
 
 export default router
