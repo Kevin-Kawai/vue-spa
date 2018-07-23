558d99e8e83b6ff1299be1aebe22eed2c80ea050
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-07-24 07:48:10 +0900

    route parameters

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -4,7 +4,8 @@
       <router-link to="/" exact>
         <img src="http://bit.ly/vue-img" alt="Vue SPA" />
       </router-link>
-      <router-link class="nav-item is-tab" to='/category/front-end' exact>Home</router-link>
+      <router-link class="nav-item is-tab" to='/category/front-end' exact>Front-end</router-link>
+      <router-link class="nav-item is-tab" to='/category/mobile' exact>Mobile</router-link>
       <router-link class="nav-item is-tab" to='/login'>Login</router-link>
     </div>
   </nav>
