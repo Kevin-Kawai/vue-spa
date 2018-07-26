7d9e8c612d6657335db771d1aeaa47237c1ae49d
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-07-27 06:42:01 +0900

    Authentication Status

--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -1,46 +1,54 @@
 <template>
 <div class="content">
-	<h2>Login</h2>
-	<div class="field is-horizontal">
-		<div class="field-label is-normal">
-		  <label class="label">Username</label>
-		</div>
-		<div class="field-body">
-		  <div class="field">
-			<div class="control">
-			  <input v-model="username" class="input" type="text"
-			  placeholder="Your username">
-			</div>
-		  </div>
-		</div>
-	</div>
-	<div class="field is-horizontal">
-		<div class="field-label is-normal">
-		  <label class="label">Password</label>
-		</div>
-		<div class="field-body">
-		  <div class="field">
-			<div class="control">
-			  <input v-model="password" class="input" type="password"
-			  placeholder="Your password">
-			</div>
-		  </div>
-		</div>
-	</div>
-	<div class="field is-horizontal">
-		<div class="field-label">
-		  <!-- Left empty for spacing -->
-		</div>
-		<div class="field-body">
-		  <div class="field">
-			<div class="control">
-			  <button v-on:click="login()" class="button is-primary">
-				Login
-			  </button>
-			</div>
-		  </div>
-		</div>
-	</div>
+  <div v-if="isAuthenticated">
+    Hello authenticated user!
+    <button v-on:click="logout()" class="button is-primary">
+      Logout
+    </button>
+  </div>
+  <div v-else>
+  	<h2>Login</h2>
+  	<div class="field is-horizontal">
+  		<div class="field-label is-normal">
+  		  <label class="label">Username</label>
+  		</div>
+  		<div class="field-body">
+  		  <div class="field">
+  			<div class="control">
+  			  <input v-model="username" class="input" type="text"
+  			  placeholder="Your username">
+  			</div>
+  		  </div>
+  		</div>
+  	</div>
+  	<div class="field is-horizontal">
+  		<div class="field-label is-normal">
+  		  <label class="label">Password</label>
+  		</div>
+  		<div class="field-body">
+  		  <div class="field">
+  			<div class="control">
+  			  <input v-model="password" class="input" type="password"
+  			  placeholder="Your password">
+  			</div>
+  		  </div>
+  		</div>
+  	</div>
+  	<div class="field is-horizontal">
+  		<div class="field-label">
+  		  <!-- Left empty for spacing -->
+  		</div>
+  		<div class="field-body">
+  		  <div class="field">
+  			<div class="control">
+  			  <button v-on:click="login()" class="button is-primary">
+  				Login
+  			  </button>
+  			</div>
+  		  </div>
+  		</div>
+  	</div>
+  </div>
 </div>
 </template>
 <script>
@@ -49,7 +57,8 @@
     data () {
       return {
         username: '',
-        password: ''
+        password: '',
+        isAuthenticated: false
       }
     },
     methods: {
@@ -58,8 +67,23 @@
           .then((data) => {
             window.localStorage.setItem('token', data.token)
             window.localStorage.setItem('tokenExpiration', data.expiration)
+            this.isAuthenticated = true
+            this.username = ''
+            this.password = ''
           })
           .catch(() => window.alert('Could not login!'))
+      },
+      logout () {
+        window.localStorage.setItem('token', null)
+        window.localStorage.setItem('tokenExpiration', null)
+        this.isAuthenticated = false
+      }
+    },
+    created () {
+      let expiration = window.localStorage.getItem('tokenExpiration')
+      var unixTimeStamp = new Date().getTime / 1000
+      if (expiration !== null && parseInt(expiration) - unixTimeStamp > 0) {
+        this.isAuthenticated = true
       }
     }
   }
