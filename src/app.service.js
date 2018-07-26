2745c34eb00647d31ad82d6be1e651147e2118f0
Kevin-Kawai <kevin.fredricks@gmail.com>
2018-07-27 06:54:29 +0900

    intercept requests

--- src/app.service.js
+++ src/app.service.js
@@ -2,10 +2,30 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'https://api.fullstackweekly.com'
 
+axios.interceptors.request.use(function (config) {
+  const token = window.localStorage.getItem('token')
+  if (typeof window === 'undefined') {
+    return config
+  }
+  if (token) {
+    config.headers.Authorization = `Bearer ${window.localStorage.getItem('token')}`
+  }
+
+  return config
+})
+
 const appService = {
   getPosts (categoryId) {
     return new Promise((resolve) => {
       axios.get(`/wp-json/wp/v2/posts?categories=${categoryId}&per_page=6`)
+        .then(response => {
+          resolve(response.data)
+        })
+    })
+  },
+  getProfile () {
+    return new Promise((resolve) => {
+      axios.get('/services/profile.php')
         .then(response => {
           resolve(response.data)
         })
--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -2,6 +2,8 @@
 <div class="content">
   <div v-if="isAuthenticated">
     Hello authenticated user!
+    <p>Name: {{profile.firstName}}</p>
+    <p>Favorite Sandwich: {{profile.favoriteSandwich}}</p>
     <button v-on:click="logout()" class="button is-primary">
       Logout
     </button>
@@ -58,7 +60,20 @@
       return {
         username: '',
         password: '',
-        isAuthenticated: false
+        isAuthenticated: false,
+        profile: {}
+      }
+    },
+    watch: {
+      isAuthenticated: function (val) {
+        if (val) {
+          appService.getProfile()
+            .then(profile => {
+              this.profile = profile
+            })
+        } else {
+          this.profile = {}
+        }
       }
     },
     methods: {
